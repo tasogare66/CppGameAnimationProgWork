cab1f57844b3598ce40deef2c4aa9fd62641898e
tasogare66 <tasogare66@gmail.com>
2024-12-08 23:44:06 +0900

    プロジェクト ファイルを追加します。

---
+++ CppGameAnimationProgWork.cpp
@@ -0,0 +1,12 @@
+﻿// CppGameAnimationProgWork.cpp : アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "CppGameAnimationProgWork.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
