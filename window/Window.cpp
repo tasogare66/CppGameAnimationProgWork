0e2d8690fb6a8e291339d32e26187a87ca842804
tasogare66 <tasogare66@gmail.com>
2024-12-09 02:51:17 +0900

    chapter01 OpenGL追加

--- window/Window.cpp
+++ window/Window.cpp
@@ -18,12 +18,25 @@ bool Window::init(unsigned int width, unsigned int height, const char* const tit
     return false;
   }
 
+  glfwMakeContextCurrent(mWindow);
+
   Logger::log(1, "%s: Window successfully initialized\n", __FUNCTION__);
   return true;
 }
 
 void Window::mainLoop() {
+  /* force VSYNC */
+  glfwSwapInterval(1);
+
+  float color = 0.0f;
   while (!glfwWindowShouldClose(mWindow)) {
+    color >= 1.0f ? color = 0.0f : color += 0.01f;
+
+    glClearColor(color, color, color, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    /* swap buffers */
+    glfwSwapBuffers(mWindow);
 
     /* poll events in a loop */
     glfwPollEvents();
