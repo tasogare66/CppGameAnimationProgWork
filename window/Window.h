e93fd33319f682897ae5d80536a7629cf2309d1e
tasogare66 <tasogare66@gmail.com>
2024-12-09 02:42:19 +0900

    chapter01 glfw追加

--- window/Window.h
+++ window/Window.h
@@ -1,12 +1,13 @@
 #pragma once
 #include <string>
+#include <GLFW/glfw3.h>
 
 class Window {
 public:
-  bool init(unsigned int width, unsigned int height, std::string title);
+  bool init(unsigned int width, unsigned int height, const char* const title);
   void mainLoop();
   void cleanup();
 
 private:
-  //GLFWwindow* mWindow = nullptr;
+  GLFWwindow* mWindow = nullptr;
 };
